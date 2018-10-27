5772c759f2811d913d338de341feeadfbf03b321
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2018-10-27 12:19:00 +0530

    Restructure project directories and files.

--- run_all_tests.sh
+++
@@ -1,4 +0,0 @@
-./run_test.sh ./create_diamond.js test/diamond_test_inputs test/diamond_test_output
-./run_test.sh ./create_triangle.js test/triangle_inputs test/triangle_outputs
-./run_test.sh ./create_rectangle.js test/rectangle_inputs test/rectangle_outputs
-
---
+++ scripts/run_app_test.sh
@@ -0,0 +1,4 @@
+./run_test.sh ../createDiamond.js ../app_test_data/diamond_test_inputs ../app_test_data/diamond_test_output
+./run_test.sh ../createTriangle.js ../app_test_data/triangle_inputs ../app_test_data/triangle_outputs
+./run_test.sh ../createRectangle.js ../app_test_data/rectangle_inputs ../app_test_data/rectangle_outputs
+
